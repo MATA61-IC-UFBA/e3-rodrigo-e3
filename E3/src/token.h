2c8b5a18a2aa14d1866736ca1c427a81d1e1ca36
rodrigovoks <rodrigoqsconceicao@hotmail.com>
2024-08-14 01:26:12 -0300

    Implementação

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,13 +1,33 @@
 /* token.h */
 
-typedef enum {
-        ID = 256, 
-	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
+typedef enum
+{
+        ID = 256,
+        NUM,
+        // Keys
+        ARRY,
+        BOOL,
+        KCHAR,
+        ELSE,
+        FLS,
+        FOR,
+        FNC,
+        IF,
+        INT,
+        PRT,
+        RTN,
+        KSTR,
+        TRUE,
+        VOID,
+        WHLE,
+        // Composite Symbols
+        LEQ,
+        GEQ,
+        EQ,
+        NEQ,
+        AND,
+        OR,
         CHAR,
-	STR,
-	ERROR
-} token_t; 
-
+        STR,
+        ERROR
+} token_t;
